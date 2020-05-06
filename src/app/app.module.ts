51b5e6af2f1bdc38bba81bb65f5a0a1301849c0b
Aaron Thiele <56663193+sirfee@users.noreply.github.com>
2020-05-06 16:44:39 +1200

    Add routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,43 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { GitSearchService } from './git-search.service';
 import { GitSearchComponent } from './git-search/git-search.component';
+import { RouterModule, Routes } from '@angular/router'; 
+import { HomePageComponent } from './home-page/home-page.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+
+const appRoutes: Routes = [
+  { path:'',
+    component: HomePageComponent
+  },
+  { path:'search',
+    component: GitSearchComponent,
+    data: {
+      title: 'Git Search'
+    }
+  },
+  { path:'**',
+    component: NotFoundComponent 
+  }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    GitSearchComponent
+    GitSearchComponent,
+    HomePageComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(
+      appRoutes
+    )
   ],
   providers: [GitSearchService],
   bootstrap: [AppComponent]
